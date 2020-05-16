581fa004275995946fd0ed7c7b316342a733fb19
성수 박 <whiteibescu@gmail.com>
2020-05-16 19:59:25 +0900

    문제번호 1000

---
+++ Problem1000/Program.cs
@@ -0,0 +1,21 @@
+﻿/*문제 
+두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+
+첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+
+첫째 줄에 A+B를 출력한다.
+
+*/
+using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        string s = Console.ReadLine();
+        string[] ss = s.Split();
+        int a = int.Parse(ss[0]);
+        int b = int.Parse(ss[1]);
+        Console.WriteLine(a + b);
+    }
+}
