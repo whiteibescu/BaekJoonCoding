ae2841910320b2f4b33db6513fcbde9da1b6eee7
성수 박 <whiteibescu@gmail.com>
2020-05-17 19:06:28 +0900

    문제번호1001

---
+++ Problem1001/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+문제 
+
+두정수 A와 B를 입력받은 다음, A-B를 출력하는 프로그램을 작성하시오.
+
+입력
+
+첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+
+출력
+첫째 줄에 A-B를 출력한다.
+*/
+
+using System;
+
+namespace Problem1001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            string[] ss = s.Split();
+            int a = int.Parse(ss[0]);
+            int b = int.Parse(ss[1]);
+            Console.WriteLine(a - b);
+        }
+    }
+}
