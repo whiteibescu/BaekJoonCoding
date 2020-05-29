92f6409e2767df445abcf4750769a089d1e5b4f4
성수 박 <whiteibescu@gmail.com>
2020-05-29 22:43:52 +0900

    별 출력 프로그램

---
+++ ProblemStar1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace ProblemStar1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("별 출력 횟수를 입력해주세요.");
+            int n = int.Parse(Console.ReadLine());
+
+
+
+            for (int i = 0; i < n; i++)
+            {
+
+                for (int j = 0; j < i + 1; j++)
+                {
+                    Console.Write("*");
+
+                }
+                Console.WriteLine();
+            }
+            
+        }
+    }
+}
