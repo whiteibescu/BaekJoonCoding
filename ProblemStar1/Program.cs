1623d0098a62ea835aabbdfe1bc38d98689f1933
성수 박 <whiteibescu@gmail.com>
2020-05-30 08:42:40 +0900

    별 출력

--- ProblemStar1/Program.cs
+++ ProblemStar1/Program.cs
@@ -12,7 +12,7 @@ namespace ProblemStar1
 
 
 
-            for (int i = 0; i < n; i++)
+            for (int i = 0; i > n; i--)
             {
 
                 for (int j = 0; j < i + 1; j++)
@@ -26,3 +26,12 @@ namespace ProblemStar1
         }
     }
 }
+/*
+문제
+첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제
+
+입력
+첫째 줄에 N(1 ≤ N ≤ 100)이 주어진다.
+
+출력
+첫째 줄부터 N번째 줄까지 차례대로 별을 출력한다.
---
+++ ProblemStar2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProblemStar2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
