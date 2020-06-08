f235f5206d7b1cd5fadab7a5dd863f4e5614f14f
성수 박 <whiteibescu@gmail.com>
2020-06-08 21:26:08 +0900

    별출력

--- ProblemStar3/Program.cs
+++ ProblemStar3/Program.cs
@@ -11,9 +11,9 @@ namespace ProblemStar2
 
             for (int i = 0; i < n; i++)
             {
-                for (int j = 0; j < n; j++)
+                for (int j = 0; j <= n; j++)
                 {
-                    if (i < 4 && j < 4 - i)
+                    if (i < n && j < n - i)
                     {
                         Console.Write(" ");
                     }
